ad3a9891cadf1c533b51e4699c60c7e9cbef192b
Angel Cordova <ae_cordova@outlook.com>
2021-09-17 17:07:12 -0500

    Set new messages as main action

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,6 +2,6 @@ module PostsHelper
   def see_comments_caption(post)
     return "No comments yet" unless post.comments.present?
     
-    "See All #{post.comments.count} Comments"
+    "See Comments [ #{post.comments.count} ]"
   end
 end
