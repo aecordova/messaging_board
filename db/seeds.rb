0f9aa644bee8d342ee550b934634b7d7425e61d5
Angel Cordova <ae_cordova@outlook.com>
2021-09-18 13:25:09 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Seeding Database...'
+puts 'Creating users...'
+users = FactoryBot.create_list(:user, 50)
+
+puts 'Creating posts...'
+users.each do |user|
+  FactoryBot.create_list(:post, rand(1...5), author: user)
+end
+
+user_ids = users.pluck(:id)
+
+puts 'Adding comments to posts...'
+Post.all.each do |post|
+  rand(1..5).times do
+    author_id = user_ids[rand(0..(user_ids.size - 1))]
+    author = User.find(author_id)
+    FactoryBot.create(:comment, author: author, post: post)
+  end
+end
+
+puts 'Done!!!'
